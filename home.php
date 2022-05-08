e31daf7200990864c6011164be18b8cd8929df6e
YoussefAmrousy <Youssef.amrousy@gmail.com>
2022-05-08 20:12:29 +0200

    Registration & Login 1.1

--- home.php
+++ home.php
@@ -1,33 +1,64 @@
 <html>
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="homeStyle.css">
-    </head>
-	<body>
-        <?php
+<head>
+  <title>Homepage</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="homeStyle.css">
+</head>
+
+<body style="height:1500px">
+<?php
         if(session_id() == '') {
             session_start();
         }
-        if (isset($_SESSION['Fname'])) {
-            echo "<div class='sessionWelcome'>Welcome ".$_SESSION['Fname']."</div><nav name='navbar'>
-            <a href='home.php'>Home</a>
-            <a href='about.html'>About</a>
-            <a href='contact.html'>Contact Us</a>
-            <a href='SignOut.php'>Sign Out</a>
-            
-		</nav>";
+        ?>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+    <a class="navbar-brand" href="#">Hurghada GRND Hotel</a>
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+        </li>
+        <?php if (isset($_SESSION['Fname'])) {
+            echo "<li class='nav-item'>
+            <a class='nav-link' href='SignOut.php'>Sign Out</a>
+            </li>";
         }
         else {
-            echo "<nav name='navbar'>
-            <a href='home.php'>Home</a>
-            <a href='about.html'>About</a>
-            <a href='login.php'>Login</a>
-            <a href='SignUp.php'>Sign Up</a>
-            <a href='contact.html'>Contact Us</a>
-		</nav>";
+            echo "<li class='nav-item'>
+            <a class='nav-link' href='Login.php'>Login</a>
+            </li>
+            <li class='nav-item'>
+            <a class='nav-link' href='SignUp.php'>Register</a>
+            </li>";
         }
         ?>
-	</body>
+        <li class="nav-item">
+            <a class="nav-link" id ="contact" href="#">Contact Us</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled " id="username" href="#"></a>
+        </li>
+        <?php if (isset($_SESSION['Fname'])) {
+            echo "<script>
+            document.getElementById('username').innerHTML = 'Welcome ".$_SESSION['Fname']."';
+            document.getElementById('username').style.visibility = 'visible';
+            document.getElementById('username').style.textAlign = right;
+            </script>";
+        }
+        else {
+            echo "<script>
+            document.getElementById('username').style.visiblity ='hidden';
+            </script>";
+        }
+        ?>
+    </ul>
+</nav>
+</body>
 </html>
