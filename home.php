503c5c7ff1940bb0c778ebeba5b6503fbf1304ae
YoussefAmrousy <Youssef.amrousy@gmail.com>
2022-05-08 20:34:59 +0200

    Registration & Login 1.2

--- home.php
+++ home.php
@@ -7,7 +7,6 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-  <link rel="stylesheet" href="homeStyle.css">
 </head>
 
 <body style="height:1500px">
@@ -27,6 +26,9 @@
         </li>
         <?php if (isset($_SESSION['Fname'])) {
             echo "<li class='nav-item'>
+            <a class='nav-link' href='#'>Book</a>
+            </li>
+            <li class='nav-item'>
             <a class='nav-link' href='SignOut.php'>Sign Out</a>
             </li>";
         }
