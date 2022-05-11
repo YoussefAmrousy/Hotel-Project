0e3716a07498fd433b3838b23734fc46e36afa41
YoussefAmrousy <Youssef.amrousy@gmail.com>
2022-05-11 04:58:39 +0200

    About Page

---
+++ About.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+    <?php include 'home.php'; ?>
+    <br><br><br>
+    <h1>About</h1>
+    <p>Hurghada GRND Hotel is one of the biggest Hotels in Hurghada. It has been operating in
+Hurghada since 1910. At this time, the guests were few and can be served while enjoying their
+stay in the Hotel. Now, the number of guests has increased and the types of the guests have
+become different.</p>
+</body>
