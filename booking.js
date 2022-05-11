7bb48960f26913d99af177ff340ab0d0075c8799
Ziad-K11 <ziadkhaled1101@gmail.com>
2022-05-11 22:21:08 +0200

    frontend for login and registration

--- booking.js
+++ booking.js
@@ -1,6 +1,8 @@
+
 function checkSelection() {
     var elem = document.getElementById("daySelect").value;
     if (elem == "I have a different plan") {
+        <br></br>
         document.getElementById("DifferentPlan").style.visibility = "visible";
         document.getElementById("dayLabel").style.visibility = "visible";
     }
