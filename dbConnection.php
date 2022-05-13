268364a741df3e4eece3f2ef8ae5bdea8e5897e0
YoussefAmrousy <Youssef.amrousy@gmail.com>
2022-05-13 05:53:49 +0200

    Connection

--- dbConnection.php
+++ dbConnection.php
@@ -3,6 +3,6 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "web-project";
-// Tables (clients, guests)
+// Tables (clients, guests, rooms)
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
