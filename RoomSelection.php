daef11c064d6dfa61ad59a968290a0720bd33733
YoussefAmrousy <Youssef.amrousy@gmail.com>
2022-05-13 05:54:42 +0200

    Room Booking

--- RoomSelection.php
+++ RoomSelection.php
@@ -6,24 +6,20 @@
     <h4>Available Rooms</h4>
     <?php
     require_once 'dbConnection.php';
+    session_Start();
     /* Available Rooms
-    10 Single Sea View
-    10 Single Pool View
-    10 Single Garden View
-    10 Double Sea View
-    10 Double Pool View
-    10 Double Garden View
-    10 Triple Sea View
-    10 Tripe Pool View 
-    10 Triple Garden View
-    Rooms Numbers: 
-    1 - 30 Single
-    31 - 70 Double
-    71 - 90 Triple */
+    10 Single Rooms
+    10 Double Rooms
+    10 Triple Rooms */
     $sql = "select TotalGuests from guests where UserID = '".$_SESSION['ID']."'";
     $result = mysqli_query($conn, $sql);
     if ($row = mysqli_fetch_array($result)) {
         echo $row['TotalGuests'];
+    }
+    unset($_SESSION['roomType']);
+    unset($_SESSION['quantity']);
+    if (isset($_SESSION['extra'])) {
+        unset($_SESSION['extra']);
     }
     ?>
     </body>
--- UserBooking.php
+++ UserBooking.php
@@ -1,9 +1,8 @@
 <html>
     <head>
+        <title>Book a room</title>
         <script src="booking.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body>
         <style>
@@ -15,43 +14,91 @@
     <br><br><br>
     <h4>Booking a room</h4>
     <form id="dayForm" method="post">
-        <h6>Days:</h6> <select id="daySelect" onchange="checkSelection()">
+        <h6>Duration Option: </h6> <select id ="daySelect" name="daySelect" onchange="checkSelection()">
             <option value="Day use">Day use</option>
             <option value="1 to 7 nights">1 to 7 nights</option>
             <option value="Long Stay">Long Stay (More than 7 nights)</option>
             <option value="Vacation">Vacation (More than a month)</option>
             <option value="I have a different plan">I have a different plan</option>
         </select><br>
-        <h6>Room Types:</h6> <select id="roomType">
+        <h6>Check in Date: </h6> <input type="date" name="checkInDate"><br>
+        <h6>Check out Date: </h6> <input type="date" name="checkOutDate"><br>                               
             <?php
             $guestNo = $_SESSION['NoOfGuests'];
-            $extra = "";
-            switch ($guestNo) {
-                case 1:
-                    echo "<option value='Single'>Single</option>";
-                    break;
-                case 2:
-                    echo "<option value='Double Room'>Double Room</option>";
-                    break;
-                case 3:
-                    echo "<option value='Triple Room'>Triple Room</option>";
-                    break;
-                case 4:
-                    echo "<option value='Triple Room'>Triple Room</option>
-                    <option value='Two double rooms'>Two double rooms</option>";
-                    break;
-                case 5:
-                    
+            if (isset($_POST['submit'])) {
+                if (empty($_POST['daySelect'])) {
+                    echo "<script>alert('Please select a duration')</script>";
+                }
+                else if (empty($_POST['checkInDate'])) {
+                    echo "<script>alert('Please enter your Check in date')</script>";
+                }
+                else if (empty($_POST['checkOutDate'])) {
+                    echo "<script>alert('Please enter your Check out date')</script>";
             }
-            ?>
-        </select>
-        <h6 id="dayLabel">Number of Days:</h6>
-        <input type="text" id="DifferentPlan" placeholder="Enter the duration">
+            $currentdate = date('dd/mm/yyyy');
+            $checkindate = new DateTime($_POST['checkInDate']);
+            $checkoutdate = new DateTime($_POST['checkOutDate']);
+            /* if ($checkindate > $currentdate) { //|| $checkoutdate > $currentdate) {
+                echo "<script>alert('This date is invalid, choose another date')</script>";
+            } */
+            if ($checkoutdate > $checkindate) {
+                echo "<script>alert('Check out date couldn't come before the Check in date'</script>";
+            }
+            else if ($checkindate == $checkoutdate) {
+                echo "<script>alert('Check in date couldn't be equal to Check out date')</script>";
+            }
+            $_SESSION['checkindate'] = $_POST['checkInDate'];
+            $_SESSION['checkoutdate'] = $_POST['checkOutDate'];
+
+            $daydiff = $checkindate->diff($checkoutdate);
+            $days = $daydiff->d;
+            echo $days;
+            $chosenDuration = $_POST['daySelect'];
+            if ($chosenDuration == 'Day use') {
+                if ($days > 1) {
+                echo "<script>alert('Duration Should be maximum 1 night')</script>"; 
+                }
+            } 
+            else if ($chosenDuration == '1 to 7 nights') {
+                if ($days > 6) {
+                echo "<script>alert('Duration should be maximum 6 nights')</script>";
+                }
+            }
+            else if ($chosenDuration == 'Long Stay') {
+                if ($days < 8) {
+                    echo "<script>alert('Duration should be at least 8 nights')</script>";
+                }
+                else if ($days > 30) {
+                    echo "<script>alert('Duration should be maximum 30 nights')</script>";
+                }
+            }
+            else if ($chosenDuration == 'Vacation') {
+                if ($days < 31) {
+                    echo "<script>alert('Duration should be at least 31 nights')</script>";
+                }
+            }
+            require_once 'dbConnection.php';
+            $sql = "SELECT DATE_FORMAT(CheckinDate, '%d/%m/%Y') FROM rooms";
+            $result = mysqli_query($conn, $sql);
+            if ($result) echo "conected";
+            if ($row = mysqli_fetch_array($result)) {
+                $checkindateRow = $row["checkinDate"];
+                $checkoutdateRow = $row["checkoutDate"];
+                if ($checkindateRow == $checkindate) {
+                    echo "<script>alert('test')</script>";
+                }
+            }
+            
+        }
+        ?>
+        <!-- To be fixed -->
+        <h6 id="dayLabel">Total Nights:</h6>
+        <input type="text" id="totalnights" name="totalnights" placeholder="Enter the duration"><br>
         <script>
-        document.getElementById("DifferentPlan").style.visibility = "hidden";
+        document.getElementById("totalnights").style.visibility = "hidden";
         document.getElementById("dayLabel").style.visibility = "hidden";
         </script>
-        <br><button onclick="location.href='RoomSelection.php'" type="button">Check Available Rooms</button>
+        <input type='submit' value='Check Available Rooms' name='submit'>
         </form>
 </body>
 </html>
