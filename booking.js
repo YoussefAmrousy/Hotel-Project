e6d99d2ab861f52b9c9489985131c27d6b8e43f9
YoussefAmrousy <Youssef.amrousy@gmail.com>
2022-05-13 05:54:10 +0200

    Room Booking

--- booking.js
+++ booking.js
@@ -2,12 +2,11 @@
 function checkSelection() {
     var elem = document.getElementById("daySelect").value;
     if (elem == "I have a different plan") {
-        <br></br>
-        document.getElementById("DifferentPlan").style.visibility = "visible";
+        document.getElementById("totalnights").style.visibility = "visible";
         document.getElementById("dayLabel").style.visibility = "visible";
     }
     else {
-        document.getElementById("DifferentPlan").style.visibility = "hidden";
+        document.getElementById("totalnights").style.visibility = "hidden";
         document.getElementById("dayLabel").style.visibility = "hidden";
     }
 }
