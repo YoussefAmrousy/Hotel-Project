089d01f1573e0032719a8b2dd18d8a17e7eee345
karimaly <66848402+karimaly@users.noreply.github.com>
2022-06-10 11:42:16 +0200

    Update About.php

--- About.php
+++ About.php
@@ -7,11 +7,17 @@
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+.mySlides {display:none;}
+</style>
 <style>
 
-body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif; color: white}
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif; color: black}
+body {background-color: white;}
 body {
- 
+
  height: 100%;
  background-position: center;
   background-repeat: no-repeat;
@@ -23,12 +29,18 @@ body {
 <header>
     <?php include 'navbar.php'; ?>
     <body>
-        <center><h1><p style="color: turquoise">Hurghada GRND hotel activities and entertainment</p></h1></center>
+    <div class="body-container">
+    <div class="landing-screen half-landing-screen background-image-section" style="background-image:linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.5)), url('./images/room_banner.jpg');">
+        <div class="landing-screen-info">
+        <center><h1 class="landing-screen-text">Hurghada GRND hotel activities and entertainment</h1></center>
+        </div>
+    </div>
+       
         <style>
             body{
                 margin: 0;
                 padding: 0;
-                background: grey;
+                background: white;
                 
             }
             .slider{
@@ -59,13 +71,96 @@ body {
         </style>
     </head>
     <body>
+    <body>
+
+<div class="w3-container">
+  <h2>We Serve you the best</h2>
+  </div>
+
+<div class="w3-content w3-display-container">
+
+<div class="w3-display-container mySlides">
+  <img src="images/amenities_chess.jpg" style=
+    "height:600px;
+    width:100%;"
+    >
+  <div class="w3-display-bottomleft w3-large w3-container w3-padding-16 w3-black">
+    Fun
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="images/about_banner.jpg" style=
+    "height:600px;
+    width:100%;">
+  <div class="w3-display-bottomright w3-large w3-container w3-padding-16 w3-black">
+    Beach
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="images/amenities_yoga.jpg" style=
+    "height:600px;
+    width:100%;">
+  <div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">
+    Relax
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="images/amenities_spa.jpg" style=
+    "height:600px;
+    width:100%;">
+  <div class="w3-display-topright w3-large w3-container w3-padding-16 w3-black">
+  Spa
+  </div>
+</div>
+
+<div class="w3-display-container mySlides">
+  <img src="images/home_restaurant.jpg" style=
+    "height:600px;
+    width:100%;">
+  <div class="w3-display-middle w3-large w3-container w3-padding-16 w3-black">
+    Restaurant
+  </div>
+</div>
+
+<button class="w3-button w3-display-left w3-black" onclick="plusDivs(-1)">&#10094;</button>
+<button class="w3-button w3-display-right w3-black" onclick="plusDivs(1)">&#10095;</button>
+
+</div>
+
+<script>
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+</script>
+
+</body>
+
         
-        <div class="slider">
         </div>
 <h3><p style="color:turquoise">Plenty of Fun and Joy Awaiting You</p></h3><br>
 Fun and joy can be found everywhere at Hurghada GRND hotel, with a wide variety of activities and entertainment facilities. Enjoy our daily live shows and daily live music that will surely delight you and add excitement to your trip. Join us during our fruit festivals and sip on a plethora of fruits, fresh juices, and cocktails. Join us during any of the following festivals: Banana Bonanza, Orange Festival, Strawberry Surprise, Sweet Potato Party, Apple Festival, Watermelon Splash, and much more!
 We also organize special carnivals and holiday events. You’ll never know what fun surprise awaits you when you stay at GRND hotel!<br>
 <h3><p style="color:turquoise">Sports and Leisure</p></h3>
+<img src="images/amenities_volleyball.jpg" style=
+"height:100%;
+    width:700px;"><br>
 Our resorts also feature multipurpose courts for a wide variety of possibilities.
 We also have numerous sports and athletic activities to take part in, including
 <ul>
@@ -79,6 +174,9 @@ We also have numerous sports and athletic activities to take part in, including
     <li>Water games and activities</li>
 </ul>
 <h3><p style="color:turquoise">Fitness</p></h3>
+<img src="images/amenities_gym.jpg" style=
+    "height:100%;
+    width:700px;"><br>
 Stay fit and in shape with our excellent fitness facilities and instructors. Our fitness programs include:
 <ul>
 <li>Aerobics </li>
@@ -87,6 +185,9 @@ Stay fit and in shape with our excellent fitness facilities and instructors. Our
 <li>Dancing</li>
 </ul>
 <h3><p style="color:turquoise">Water Games and Activities</p></h3>
+<img src="images/amenities_surfing.jpg" style=
+"height:100%;
+    width:700px;"><br>
 With the shore right at your doorstep, the water invites you to a world of fun, excitement, and adventure.
 <ul>
 <li>Sailing </li>
@@ -96,6 +197,7 @@ With the shore right at your doorstep, the water invites you to a world of fun, 
 <li>Boat trips: yachts and glass boats</li>
 <li>Snorkeling </li>
 </ul>
+<div class="slider">
     </body>
 </html>
 
