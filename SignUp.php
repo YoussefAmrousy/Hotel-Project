fc73d03c4529036a9b73ecf828b0ded881435cbb
YoussefAmrousy <Youssef.amrousy@gmail.com>
2022-06-10 19:13:13 +0200

    updates

--- SignUp.php
+++ SignUp.php
@@ -118,9 +118,6 @@ body {
         <div class="form-group">
         	<input type="email" class="form-control" name="Email" id="Email" placeholder="Email" required="required">
         </div>
-		<div class="form-group">
-            <input type="text" class="form-control" id='Address' name="Address" placeholder="Enter your Address" required="required">
-        </div>
 		Profile Picture:
 		<div class="form-group">
             <input type="file"  name="Profilepic">
@@ -165,14 +162,17 @@ if(isset($_POST['Submit'])) {
 	session_unset();
 	setcookie("Email", "", time() - 10000);
 	setcookie("Password", "", time() - 10000);
-	// $sql = "INSERT INTO users(FirstName, LastName, Email, Password, Address, NationID) 
-	// values('".$_POST['Fname']."','".$_POST['Lname']."','".$_POST['Email']."','".$_POST['Password']."','".$_POST['Address']."','".$_POST['Nationalid']."')";
-	$sql = "INSERT INTO users(FirstName, LastName, Email, Password, Address, NationID) values('Ahmed', 'Sayed', 'ahmed@gmail.com', '12345', 'eee', '1234567')";
+	$firstname = $_POST['Fname'];
+	$lastname = $_POST['Lname'];
+	$email = $_POST['Email'];
+	$password = $_POST['Password'];
+	$nationid = $_POST['NationID'];
+	$sql = "INSERT INTO users(FirstName, LastName, Email, Password, NationID) values('".$firstname."','".$lastname."','".$email."','".$password."','".$nationid."')";
 	$result = mysqli_query($conn, $sql);
 	if ($result) {
 		echo "<script>
-		alert('Registered Successfully');
-		window.location.href ='login.php';
+		alert('Registered Successfully, Please wait until the recepionist accept your request');
+		window.location.href ='Login.php';
 		</script>";
 	}
 	// if (!preg_match("/^[a-zA-Z-']*$/",$_POST['Fname']) || !preg_match("/^[a-zA-Z-']*$/",$_POST['Lname'])) {
