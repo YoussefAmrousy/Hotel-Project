e1652f85c9f19dc391b5f63f3b2061f34c54ed35
karimaly <66848402+karimaly@users.noreply.github.com>
2022-06-10 12:40:31 +0200

    Update Rooms.php

--- Rooms.php
+++ Rooms.php
@@ -8,38 +8,27 @@
     </head>
 <header>
     <?php include 'navbar.php'; ?>
-<div class="body-container">
+
     <div class="landing-screen half-landing-screen background-image-section" style="background-image:linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.5)), url('./images/room_banner.jpg');">
         <div class="landing-screen-info">
             <center><h1 class="landing-screen-text">Hurghada GRND Hotel</h1></center>
         </div>
     </div>
-    <div class="main-container">
-        <div class="main-inner-container">
-            <div class="individual-room white-container">
-                
-                <div class="individual-room-info">
-                    <div class="individual-room-type">
+    
+       <div>
                         <h2 class="large-text">Balcony Room</h2>
                         <h4>Price: $100/night</h4>
-                    </div>                    
-                </div>
-            </div>
-            <div class="individual-room-image">
-                <div class="room-image-inner-div">
+                    
+            
+            
                     <img src="./images/balcony_room.jpg" style=
     "height:600px;
     width:600px;"> 
-                </div>
-            </div>
-        </div>                
-    </div>
+                          
+    <br>
     
 
-    <div class="individual-room-outer-desc-container main-container white-container">
-        <div class="main-inner-container">
-            <div class="individual-room-desc-box white-container">
-                <div class="individual-room-desc">
+    
                     <h3 class="large-text">Description</h3>
                     <h5>Bed: 1 single bed</h5>
                     <h5>Occupancy: 1 adult</h5>
@@ -47,30 +36,16 @@
                     <h5>Bathroom: Shower Bath & Bathtub </h5>
                    
                 </div>
-                <div class="main-container">
-        <div class="main-inner-container">
-            <div class="individual-room white-container">
-                
-                <div class="individual-room-info">
-                    <div class="individual-room-type">
+                <div>
                         <h2 class="large-text">Single Room</h2>
                         <h4>Price: $50/night</h4>
-                    </div>                    
-                </div>
-            </div>
-            <div class="individual-room-image">
-                <div class="room-image-inner-div">
+                    
                     <img src="./images/classic_room.jpg" style=
     "height:600px;
     width:600px;">
-                </div>
-            </div>
-        </div>                
-    </div>
-    <div class="individual-room-outer-desc-container main-container white-container">
-        <div class="main-inner-container">
-            <div class="individual-room-desc-box white-container">
-                <div class="individual-room-desc">
+                           
+    
+    <br>
                     <h3 class="large-text">Description</h3>
                     <h5>Bed: 1 single bed</h5>
                     <h5>Occupancy: 1 adult</h5>
@@ -78,32 +53,17 @@
                     <h5>Bathroom: Shower Bath</h5>
                    
                 </div>
-                <div class="main-container">
-        <div class="main-inner-container">
-            <div class="individual-room white-container">
-                
-                <div class="individual-room-info">
-                    <div class="individual-room-type">
+                <div>
                         <h2 class="large-text">Double Room</h2>
                         <h4>Price: $80/night</h4>
-                    </div>                    
-                </div>
-            </div>
-            <div class="individual-room-image">
-                <div class="room-image-inner-div">
+                    
                     <img src="./images/home_gallery4.jpg" style=
     "height:600px;
     width:600px;"> 
-                </div>
-            </div>
-        </div>                
-    </div>
+                <br>
     
 
-    <div class="individual-room-outer-desc-container main-container white-container">
-        <div class="main-inner-container">
-            <div class="individual-room-desc-box white-container">
-                <div class="individual-room-desc">
+    
                     <h3 class="large-text">Description</h3>
                     <h5>Bed: 2 single beds</h5>
                     <h5>Occupancy: 2 adult</h5>
@@ -111,31 +71,16 @@
                     <h5>Bathroom: Shower Bath</h5>
                    
                 </div>
-                <div class="main-container">
-        <div class="main-inner-container">
-            <div class="individual-room white-container">
-                
-                <div class="individual-room-info">
-                    <div class="individual-room-type">
+                <div>
                         <h2 class="large-text">Triple Room</h2>
                         <h4>Price: $100/night</h4>
-                    </div>                    
-                </div>
-            </div>
-            <div class="individual-room-image">
-                <div class="room-image-inner-div">
+                   
                     <img src="./images/family_room.jpg" style=
     "height:600px;
     width:600px;">
-                </div>
-            </div>
-        </div>                
-    </div>
+                <br>
     
-                <div class="individual-room-outer-desc-container main-container white-container">
-        <div class="main-inner-container">
-            <div class="individual-room-desc-box white-container">
-                <div class="individual-room-desc">
+                
                     <h3 class="large-text">Description</h3>
                     <h5>Bed: 3 single beds</h5>
                     <h5>Occupancy: 3 adult</h5>
@@ -144,7 +89,7 @@
                    
                 </div>
                 
-                <div class="individual-room-services"><br>
+                <div><br>
                     <h1 class="large-text">Rooms Amenities</h1>
                     <ul>
                         <li><span class="iconify" data-icon="bi:arrow-right-short" data-inline="false"></span> Private Bathroom</li>
